3e350260bf3c4adab699dfcac9af88e270bd36eb
nicolashedoire <nicolashedoire@hotmail.com>
2020-05-29 23:40:12 +0200

    add dashboard page template

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Layout from '../../components/Layout';
+import Card from '../../components/Card';
+import styles from './styles.module.scss';
+import { Button } from 'reactstrap';
+import { hikings } from '../../config/data';
+import { NavLink } from 'react-router-dom';
 
 export default function Dashboard() {
+
     return (
-        <div>Dashboard</div>
+        <Layout filters={false}>
+            <div className={`${styles.search} mt-4`}>
+            </div>
+        </Layout>
     )
 }
