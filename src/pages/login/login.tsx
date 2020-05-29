1e11c67648a558ff5fff21f4de5451d020af5c8e
nicolashedoire <nicolashedoire@hotmail.com>
2020-05-29 17:09:36 +0200

    add google button

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Button } from 'reactstrap';
+import './styles.scss';
 
 export default function Login() {
- return (
-    <div>Login page</div>
- )
+   return (
+      <div className="login">
+         <h1>Good morning vietnam</h1>
+         <Button className="google-button">
+            <img src="/images/google.svg" alt="google" width="30" />
+              &nbsp;Se connecter avec Google
+            </Button>
+      </div>
+   )
 }
