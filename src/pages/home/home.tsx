44e9eebde2820d73b95af3d97463c7dad783f799
Unknown <unknown@example.com>
2020-05-29 18:18:26 +0200

    upadte styles in module

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,63 +1,72 @@
-import React from 'react';
+import React from "react";
 import Navbar from "../../components/Navbar";
-import "./styles.scss"
-
+import styles from "./styles.module.scss";
 
 export default function Home() {
-
-    return (
-      <div className="App">
-      <header className="header">
+  return (
+    <div className="App">
+      <header className={styles.header}>
         <Navbar />
         <h1>Walking feet</h1>
-        <p className="intro">
-          <span className="walkingfeet">Walking feet</span> est une plateforme communautaire gratuite vous proposant
-          des balades et randonnées partout dans le monde. Il n’est pas toujours évident de trouver des informations sur le net. Quoi de mieux qu’un
-          site qui les regroupe pour vous!
+        <p className={styles.intro}>
+          <span className="walkingfeet">Walking feet</span> est une plateforme
+          communautaire gratuite vous proposant des balades et randonnées
+          partout dans le monde. Il n’est pas toujours évident de trouver des
+          informations sur le net. Quoi de mieux qu’un site qui les regroupe
+          pour vous!
         </p>
-        <div className="search-container">
+        <div className={styles.searchContainer}>
           <input placeholder="Rechercher une randonnée" />
-         
         </div>
       </header>
-      <div>
-        
-        <h2>Découverte</h2>
-        <p className="informations">
-          Découvrez des lieux agréables ou des endroits insolites, un peu
-          partout dans le monde ou à 2 pas de chez vous.
-        </p>
-        <h2>Inspiration</h2>
-        <p className="informations">
-          Elargissez vos horizons et inspirez les autres utilisateurs. Grâce à 
-           <span className="walkingfeet"> Walking Feet</span>, partagez vos randonnées et découvrez de nouveaux
-          parcours.
-        </p>
-        <h2>Personnalisation</h2>
-        <p className="informations">
-          Vous aimez vous balader ou vous êtes un randonneur chevronné en quête
-          d’inspiration ? <br></br>Seuls, en groupe ou avec vos enfants ? Toute une série
-          de critères vous aideront à trouver ce qui vous correspond le mieux.
-        </p>
-        <h2>Échange </h2>
-        <p className="informations">
-          Soyez acteurs de la plateforme en commentant les randonnées que vous
-          aurez découvertes grâce aux autres utilisateurs. <br></br>Evaluez votre
-          expérience et complétez le contenu. Invitez vos proches à marcher avec
-          vous.
-        </p>
-        <h2>Carnet de bord</h2>
-        <p className="informations">
+
+      <div className={styles.section}>
+        <div>
+          <h2 className={styles.title}>Découverte</h2>
+          <p className={styles.informations}>
+            Découvrez des lieux agréables ou des endroits insolites, un peu
+            partout dans le monde ou à 2 pas de chez vous.
+          </p>
+        </div>
+        <div>
+          <h2 className={styles.title}>Inspiration</h2>
+          <p className={styles.informations}>
+            Elargissez vos horizons et inspirez les autres utilisateurs. Grâce à
+            <span className="walkingfeet"> Walking Feet</span>, partagez vos
+            randonnées et découvrez de nouveaux parcours.
+          </p>
+        </div>
+        <div>
+          <h2 className={styles.title}>Personnalisation</h2>
+          <p className={styles.informations}>
+            Vous aimez vous balader ou vous êtes un randonneur chevronné en
+            quête d’inspiration ? <br></br>Seuls, en groupe ou avec vos enfants
+            ? Toute une série de critères vous aideront à trouver ce qui vous
+            correspond le mieux.
+          </p>
+        </div>
+        <div>
+          <h2 className={styles.title}>Échange </h2>
+          <p className={styles.informations}>
+            Soyez acteurs de la plateforme en commentant les randonnées que vous
+            aurez découvertes grâce aux autres utilisateurs. <br></br>Evaluez
+            votre expérience et complétez le contenu. Invitez vos proches à
+            marcher avec vous.
+          </p>
+        </div>
+        <div> 
+        <h2 className={styles.title}>Carnet de bord</h2>
+        <p className={styles.informations}>
           Grâce à votre carnet de bord, créez un historique de vos balades,
           monitorez vos performances et partagez vos marches préférées.
           <br></br>Planifiez aussi vos prochaines randonnées.
         </p>
-        <h3>Boite à idées</h3>
-        <p className="informations">
-          Aidez-nous à améliorer notre plateforme. 
-          Envoyez un mail via cet
+        </div>
+        <h3 className={styles.title}>Boite à idées</h3>
+        <p className={styles.informations}>
+          Aidez-nous à améliorer notre plateforme. Envoyez un mail via cet
         </p>
       </div>
     </div>
-    )
+  );
 }
