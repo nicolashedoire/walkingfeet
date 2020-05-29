2ee7db4e5f68ccab601404c7d19c2d5d46f46f9c
nicolashedoire <nicolashedoire@hotmail.com>
2020-05-29 19:44:10 +0200

    fix styles in login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import { Button } from 'reactstrap';
 import { Form, FormGroup, Input } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
-import './styles.scss';
+import styles from './styles.module.scss';
 
 export default function Login() {
    return (
-      <div className="login">
+      <div className={styles.login}>
          <h1>Good morning vietnam</h1>
-         <Button className="google-button mt-4">
+         <Button className={`${styles.googleButton} mt-4`}>
             <img src="/images/google.svg" alt="google" width="30" />
             <span>Se connecter avec Google</span>
          </Button>
 
-         <div className="divider-container">
-            <div className="divider"></div>
+         <div className={styles.dividerContainer}>
+            <div className={styles.divider}></div>
             <p>ou</p>
-            <div className="divider"></div>
+            <div className={styles.divider}></div>
          </div>
          <Form>
       <FormGroup>
@@ -25,9 +25,9 @@ export default function Login() {
         <FormGroup className="mt-4">
         <Input type="password" name="password" id="password" placeholder="password" autoComplete="off" />
         </FormGroup>
-        <FormGroup className="mt-4 actions-container">
+        <FormGroup className={`mt-4 ${styles.actionsContainer}`}>
            <Button className="mr-4">Se connecter</Button>
-           <NavLink to="/forgot-password" className="forgot-password">Mot de passe oublié</NavLink>
+           <NavLink to="/forgot-password" className={styles.forgotPassword}>Mot de passe oublié</NavLink>
            </FormGroup>
         </Form>
       </div>
