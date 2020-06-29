a0213141d526b570148c4e59464726a24e861937
nicolashedoire <nicolashedoire@hotmail.com>
2020-06-29 02:07:07 +0200

    add test

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -32,9 +32,7 @@ export default function Home() {
 
   const searchCity = (event: any) => {
     setCity(event.target.value);
-    alert('toto')
     getCitiesApi(event.target.value).then((res) => {
-      alert('API call')
       setCities(res.data);
     }).catch(error => {
       alert(error.response.data);
